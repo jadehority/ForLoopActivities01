b1eb549a6bc036bf332478305cabbdd67e84a38d
mrsidell <jsidell@nohum.k12.ca.us>
2024-10-24 10:09:49 -0700

    Create ForLoopActivities01.java

---
+++ ForLoopActivities01.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+
+/**********************
+* FOR LOOP ACTIVITIES *
+**********************
+Assignment: Write code that uses a SEPARATE for loop to accomplish each of the following tasks. Print two new lines after each problem to separate its output from the next one.(25 points)
+*/
+
+// 1) Print the name of your favorite dessert three times (2 points)
+
+System.out.println();
+System.out.println();
+// 2) Print a $ 10 times on one line (2 points)
+
+System.out.println();
+System.out.println();
+// 3) Print 10 $ each on a different line (2 points)
+ 
+System.out.println();
+System.out.println();
+
+// 4) Print the numbers 1-20 on one line (2 points)
+
+System.out.println();
+System.out.println();
+// 5) Print the numbers 20-25 on one line (2 points)
+
+    
+System.out.println();
+System.out.println();
+    
+// 6) Print the numbers 10-1 (in decreasing order) on one line (2 points)
+
+    
+System.out.println();
+System.out.println();
+    
+// 7) Print the numbers -20 through -10 (in increasing order) on one line (2 points)
+
+System.out.println();
+System.out.println();
+    
+// 8) Count from 5 to 100 by 5s, printing each number on the same line. Print spaces between the numbers.  (3 points)
+
+    
+System.out.println();
+System.out.println();
+// The next two will require use of Math methods
+
+// Print the square of all numbers from 1-20 on one line. Use Math.pow() to square a number. Print spaces between the numbers. (3 points)
+
+    
+    System.out.println();
+    System.out.println();
+    
+// Print the square root of all numbers from 1-20. Use Math.sqrt() to get the square root of the number.
+System.out.println("i | SQUARE ROOT");
+System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+
+
+System.out.println();
+System.out.println();
+// Print 100 random integers between 1-10 in the console (5 points)
+
+
+/************
+* CHALLENGE *
+*************
+Write a program that asks the user how many random numbers they want.
+Then print that many random integers, each on a new line.
+*/
+
+  
+  } //end main
+} //end Main
